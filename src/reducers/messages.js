0f1b41a3cfe1bb702a2e77bbb6babba13aff912c
vanelizarov <elizarov.vanya@gmail.com>
2017-05-24 07:06:35 +0300

    Added dumb messages to default state

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -3,7 +3,45 @@ import * as types from '../actions/types';
 const defaultState = {
     userId: 1,
     list:  [
-
+        {
+            from: 1,
+            text: 'можете проверить мой счет? Я вчера им пользовался',
+            date: 1501707600000,
+            transaction: null
+        },
+        {
+            from: 1,
+            text: 'спасибо большое за помощь!',
+            date: 1501707600000,
+            transaction: null
+        },
+        {
+            from: 0,
+            text: 'здравствуйте! К сожалению, никак',
+            date: 1501707600000,
+            transaction: null
+        },
+        {
+            from: 1,
+            text: 'привет. А как перейти на тариф, на котором самый большой процент на остаток по счетам?',
+            date: 1501707600000,
+            transaction: null
+        },
+        {
+            from: 0,
+            text: '',
+            date: 1501707600000,
+            transaction: {
+                billId: 57890456,
+                operationId: 'wv31RdN'
+            }
+        },
+        {
+            from: 1,
+            text: 'приветствую. Пришлите, пожалуйста, последнюю операцию по долларовому счету',
+            date: 1501707600000,
+            transaction: null
+        }
     ],
     shadow: false
 };
